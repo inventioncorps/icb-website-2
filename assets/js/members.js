0a248fa0dc156207525f81a733adbcc5b0143a9b
Anika Singhania <anikasinghania@berkeley.edu>
2020-08-01 12:32:14 -0700

    add copies of v1.5 files

---
+++ assets/js/members.js
@@ -0,0 +1,409 @@
+$(document).ready(function () {
+  // Abhi
+  $("#Abhinav_15").hover(function(){
+    $("#abhi").addClass("underline");
+    // $(".artpiece.colored").addClass("clip");
+  }, 
+  function(){
+    $("#abhi").removeClass("underline");
+    // $(".artpiece.colored").removeClass("clip");
+  });
+
+  // Allan
+  $("#Allan_24").hover(function(){
+    $("#allan").addClass("underline");
+
+  },
+  function(){
+    $("#allan").removeClass("underline");
+  });
+
+  // $("#allan").hover(function(){
+  //   $("#Allan_24").css("opacity", "100%");
+
+  // },
+  // function(){
+  //   $("#Allan_24").css("opacity", "0%");
+  // });
+
+  // Anderson
+  $("#Anderson_30").hover(function(){
+    $("#anderson").addClass("underline");
+
+  },
+  function(){
+    $("#anderson").removeClass("underline");
+  });
+
+  // Anika
+  $("#Anika_8").hover(function(){
+    $("#anika").addClass("underline");
+  },
+  function(){
+    $("#anika").removeClass("underline");
+  });
+
+  // Anjali
+  $("#Anjali_43").hover(function(){
+    $("#anjali").addClass("underline");
+  },
+  function(){
+    $("#anjali").removeClass("underline");
+  });
+  
+  // Aryan
+  $("#Aryan_27").hover(function(){
+    $("#aryan").addClass("underline");
+  },
+  function(){
+    $("#aryan").removeClass("underline");
+  });
+  
+  // Batu
+  $("#Batu_16").hover(function(){
+    $("#batu").addClass("underline");
+  },
+  function(){
+    $("#batu").removeClass("underline");
+  });
+
+  // Bongi
+  $("#Bongi_23").hover(function(){
+    $("#bongi").addClass("underline");
+  },
+  function(){
+    $("#bongi").removeClass("underline");
+  });
+
+  // Brendan
+  $("#Brendan_1").hover(function(){
+    $("#brendan").addClass("underline");
+  },
+  function(){
+    $("#brendan").removeClass("underline");
+  });
+
+  // Cait
+  $("#Caitlin_34").hover(function(){
+    $("#cait").addClass("underline");
+  },
+  function(){
+    $("#cait").removeClass("underline");
+  });
+
+  // Carlos
+  $("#Carlos_44").hover(function(){
+    $("#carlos").addClass("underline");
+  },
+  function(){
+    $("#carlos").removeClass("underline");
+  });
+
+  // Courtnie
+  $("#Courtnie_39").hover(function(){
+    $("#courtnie").addClass("underline");
+  },
+  function(){
+    $("#courtnie").removeClass("underline");
+  });
+
+  // Dan
+  $("#Daniel_28").hover(function(){
+    $("#dan").addClass("underline");
+  },
+  function(){
+    $("#dan").removeClass("underline");
+  });
+
+  // Grace
+  $("#Grace_11").hover(function(){
+    $("#grace").addClass("underline");
+  },
+  function(){
+    $("#grace").removeClass("underline");
+  });
+
+  // Hannah
+  $("#Hannah_33").hover(function(){
+    $("#hannah").addClass("underline");
+  },
+  function(){
+    $("#hannah").removeClass("underline");
+  });
+
+  // Iris
+  $("#Iris_41").hover(function(){
+    $("#iris").addClass("underline");
+  },
+  function(){
+    $("#iris").removeClass("underline");
+  });
+
+  // Isabella
+  $("#Isabella_9").hover(function(){
+    $("#isabella").addClass("underline");
+  },
+  function(){
+    $("#isabella").removeClass("underline");
+  });
+
+  // Jake
+  $("#Jake_35").hover(function(){
+    $("#jake").addClass("underline");
+  },
+  function(){
+    $("#jake").removeClass("underline");
+  });
+
+  // Jasmine
+  $("#Jasmine_37").hover(function(){
+    $("#jasmine").addClass("underline");
+  },
+  function(){
+    $("#jasmine").removeClass("underline");
+  });
+
+  // Jeffrey
+  $("#Jeffrey_20").hover(function(){
+    $("#jeffrey").addClass("underline");
+  },
+  function(){
+    $("#jeffrey").removeClass("underline");
+  });
+
+  // Joe 
+  $("#Joe_12").hover(function(){
+    $("#joe").addClass("underline");
+  },
+  function(){
+    $("#joe").removeClass("underline");
+  });
+
+  // Joey
+  $("#Joseph_14").hover(function(){
+    $("#joey").addClass("underline");
+  },
+  function(){
+    $("#joey").removeClass("underline");
+  });
+
+  // Katrina
+  $("#Katrina_17").hover(function(){
+    $("#katrina").addClass("underline");
+  },
+  function(){
+    $("#katrina").removeClass("underline");
+  });
+
+  // Kaylan
+  $("#Kaylan_10").hover(function(){
+    $("#kaylan").addClass("underline");
+  },
+  function(){
+    $("#kaylan").removeClass("underline");
+  });
+
+  // Laurel
+  $("#Laurel_40").hover(function(){
+    $("#laurel").addClass("underline");
+  },
+  function(){
+    $("#laurel").removeClass("underline");
+  });
+
+  // Leilani
+  $("#Leilani_13").hover(function(){
+    $("#leilani").addClass("underline");
+  },
+  function(){
+    $("#leilani").removeClass("underline");
+  });
+
+  // Leo
+  $("#Leo_36").hover(function(){
+    $("#leo").addClass("underline");
+  },
+  function(){
+    $("#leo").removeClass("underline");
+  });
+
+  // Malena
+  $("#Malena_7").hover(function(){
+    $("#malena").addClass("underline");
+  },
+  function(){
+    $("#malena").removeClass("underline");
+  });
+
+  // Manooshree
+  $("#Manooshree_49").hover(function(){
+    $("#manooshree").addClass("underline");
+  },
+  function(){
+    $("#manooshree").removeClass("underline");
+  });
+
+  // Maya
+  $("#Maya_19").hover(function(){
+    $("#maya").addClass("underline");
+  },
+  function(){
+    $("#maya").removeClass("underline");
+  });
+
+  // Michael
+  $("#Michael_21").hover(function(){
+    $("#michael").addClass("underline");
+  },
+  function(){
+    $("#michael").removeClass("underline");
+  });
+
+  // Natalie C
+  $("#Natalie_C._6").hover(function(){
+    $("#natalie_c").addClass("underline");
+  },
+  function(){
+    $("#natalie_c").removeClass("underline");
+  });
+
+  // Natalie D
+  $("#Natalie_D._3").hover(function(){
+    $("#natalie_d").addClass("underline");
+  },
+  function(){
+    $("#natalie_d").removeClass("underline");
+  });
+
+  // Neema
+  $("#Neema_45").hover(function(){
+    $("#neema").addClass("underline");
+  },
+  function(){
+    $("#neema").removeClass("underline");
+  });
+
+  // Neha
+  $("#Neha_38").hover(function(){
+    $("#neha").addClass("underline");
+  },
+  function(){
+    $("#neha").removeClass("underline");
+  });
+
+  // Nicole
+  $("#Nicole_26").hover(function(){
+    $("#nicole").addClass("underline");
+  },
+  function(){
+    $("#nicole").removeClass("underline");
+  });
+
+  // Nik
+  $("#Nikhil_31").hover(function(){
+    $("#nik").addClass("underline");
+  },
+  function(){
+    $("#nik").removeClass("underline");
+  });
+
+  // Philippa
+  $("#Philippa_32").hover(function(){
+    $("#philippa").addClass("underline");
+  },
+  function(){
+    $("#philippa").removeClass("underline");
+  });
+
+  // Qianning
+  $("#Qianning_46").hover(function(){
+    $("#qianning").addClass("underline");
+  },
+  function(){
+    $("#qianning").removeClass("underline");
+  });
+
+  // Regan 
+  $("#Reagan_48").hover(function(){
+    $("#regan").addClass("underline");
+  },
+  function(){
+    $("#regan").removeClass("underline");
+  });
+
+  // Reid
+  $("#Reid_29").hover(function(){
+    $("#reid").addClass("underline");
+  },
+  function(){
+    $("#reid").removeClass("underline");
+  });
+
+  // Ryan
+  $("#Ryan_2").hover(function(){
+    $("#ryan").addClass("underline");
+  },
+  function(){
+    $("#ryan").removeClass("underline");
+  });
+
+  // Shaantam
+  $("#Shaantam_5").hover(function(){
+    $("#shaantam").addClass("underline");
+  },
+  function(){
+    $("#shaantam").removeClass("underline");
+  });
+
+  // Sunny
+  $("#Sunny_4").hover(function(){
+    $("#sunny").addClass("underline");
+  },
+  function(){
+    $("#sunny").removeClass("underline");
+  });
+
+  // Tanthai
+  $("#Tanthai_22").hover(function(){
+    $("#tanthai").addClass("underline");
+  },
+  function(){
+    $("#tanthai").removeClass("underline");
+  });
+
+  // Tori
+  $("#Victoria_18").hover(function(){
+    $("#tori").addClass("underline");
+  },
+  function(){
+    $("#tori").removeClass("underline");
+  });
+
+  // Vince
+  $("#Vince_47").hover(function(){
+    $("#vince").addClass("underline");
+  },
+  function(){
+    $("#vince").removeClass("underline");
+  });
+
+  // Zeyad
+  $("#Zeyad_42").hover(function(){
+    $("#zeyad").addClass("underline");
+  },
+  function(){
+    $("#zeyad").removeClass("underline");
+  });
+
+  // Zulaika
+  $("#Zulaika_25").hover(function(){
+    $("#zulaika").addClass("underline");
+  },
+  function(){
+    $("#zulaika").removeClass("underline");
+  });
+});
+
+// $(function () {
+//   $('[data-toggle="tooltip"]').tooltip()
+// })
---
+++ assets/js/script.js
@@ -0,0 +1,60 @@
+// Make nav background white instead of transparent after scrolling
+$(function () {
+    $(document).scroll(function () {
+      var $nav = $(".navbar");
+      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+      // $(".navbar-brand>img").attr("width","30px");
+      // $(".navbar-brand>img").attr("height","30px");
+    });
+  });
+
+
+$(document).ready(function () {
+    // Update nav to show where you are
+    $(document).on("scroll", onScroll);
+
+    // var url = window.location;
+    // $('ul.nav a[href="'+ url +'"]').parent().addClass('active');
+    // $('ul.nav a').filter(function() {
+    //      return this.href == url;
+    // }).parent().addClass('active');
+});
+
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('#navbarNav a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('navbar-nav li a').removeClass("active");
+            currLink.addClass("active");
+        }
+        else{
+            currLink.removeClass("active");
+        }
+    });
+}
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  });
+
+//Using basicScroll to create parallax scrolling on landing
+document.querySelectorAll('.scene').forEach((elem) => {
+  
+  const modifier = elem.getAttribute('data-modifier')
+  
+  basicScroll.create({
+    elem: elem,
+    from: 0,
+    to: 5000,
+    direct: true,
+    props: {
+      '--translateY': {
+        from: '0',
+        to: `${ 10 * modifier }px`
+      }
+    }
+  }).start()
+    
+});
